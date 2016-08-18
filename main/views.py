024f9d84970f5d9ba5f6d88f8ab04b84560f2be7
Daryl Ebata <daryl.ebata@gmail.com>
2016-08-17 20:26:39 -0400

    Testing out linter plugin and fixing generated warnings

--- main/views.py
+++ main/views.py
@@ -3,19 +3,26 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from main.models import Version, TestCase, TestCharter, TestGroup, Persona, Defect, TestResult, TestSession
-from main.forms import VersionForm, TestCaseForm, TestCharterForm, TestGroupForm, PersonaForm, DefectForm, TestResultForm, TestSessionForm
+from main.models import (Version, TestCase, TestCharter,
+                         TestGroup, Persona, Defect,
+                         TestResult, TestSession)
+from main.forms import (VersionForm, TestCaseForm,
+                        TestCharterForm, TestGroupForm,
+                        PersonaForm, DefectForm,
+                        TestResultForm, TestSessionForm)
+
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             return redirect(home)
     else:
         form = UserCreationForm()
-    return render(request, 'main/register_user.html', {'form':form})
+    return render(request, 'main/register_user.html', {'form': form})
+
 
 @login_required
 def home(request):
@@ -30,18 +37,19 @@ def home(request):
 def show_test_cases(request, version_id=None):
     context = dict()
     if version_id:
-        context['all_test_cases'] = TestCase.objects.filter(versions__id=version_id)
+        context['all_test_cases'] = TestCase.objects.filter(
+                versions__id=version_id)
     else:
         context['all_test_cases'] = TestCase.objects.all()
-    return render(request, "main/test_cases.html",
-    context)
+    return render(request, "main/test_cases.html", context)
+
 
 @login_required
 def show_test_case(request, test_id):
     context = dict()
     context['test_case'] = TestCase.objects.get(id=test_id)
-    return render(request, "main/test_case_details.html",
-    context)
+    return render(request, "main/test_case_details.html", context)
+
 
 @login_required
 def new_test_case(request):
@@ -60,6 +68,7 @@ def new_test_case(request):
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def update_test_case(request, id):
     context = dict()
@@ -74,17 +83,19 @@ def update_test_case(request, id):
             form.save_m2m()
             return redirect(show_test_cases)
     else:
-        form = TestCaseForm(instance = test_case)
+        form = TestCaseForm(instance=test_case)
     context['form'] = form
     context['test_case'] = test_case
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_test_case(request, id):
-    test_case  = TestCase.objects.get(id=id)
+    test_case = TestCase.objects.get(id=id)
     if test_case:
         test_case.delete()
     return redirect(show_test_cases)
+
 
 @login_required
 def run_test_case(request, test_group_id, test_case_id):
@@ -100,15 +111,18 @@ def run_test_case(request, test_group_id, test_case_id):
             test_result.execution_date = datetime.datetime.now()
             test_result.save()
             form.save_m2m()
-            return show_test_group(request,test_group_id)
+            return show_test_group(request, test_group_id)
     else:
-        tr = TestResult.objects.filter(test_case__id=test_case_id, test_group__id=test_group_id).exists()
+        tr = TestResult.objects.filter(test_case__id=test_case_id,
+                                       test_group__id=test_group_id).exists()
         if not tr:
             form = TestResultForm()
         else:
-            return HttpResponse('Error - Test result has already been recordered for this test case')
+            return HttpResponse('Error - Test result has already been'
+                                'recordered for this test case')
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
+
 
 @login_required
 def run_test_charter(request, test_group_id, test_charter_id):
@@ -118,21 +132,25 @@ def run_test_charter(request, test_group_id, test_charter_id):
         form = TestSessionForm(request.POST)
         if form.is_valid():
             test_session = form.save(commit=False)
-            test_session.test_charter = TestCharter.objects.get(id=test_charter_id)
+            test_session.test_charter = TestCharter.objects.get(
+                    id=test_charter_id)
             test_session.test_group = TestGroup.objects.get(id=test_group_id)
             test_session.tester = request.user
             test_session.execution_date = datetime.datetime.now()
             test_session.save()
             form.save_m2m()
-            return show_test_group(request,test_group_id)
+            return show_test_group(request, test_group_id)
     else:
-        tr = TestSession.objects.filter(test_charter__id=test_charter_id, test_group__id=test_group_id).exists()
+        tr = TestSession.objects.filter(test_charter__id=test_charter_id,
+                                        test_group__id=test_group_id).exists()
         if not tr:
             form = TestSessionForm()
         else:
-            return HttpResponse('Error - Test session has already been recordered for this test case')
+            return HttpResponse('Error - Test session has already been'
+                                'recordered for this test case')
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
+
 
 @login_required
 def view_test_result(request, result_id=None):
@@ -141,6 +159,7 @@ def view_test_result(request, result_id=None):
         context['result'] = TestResult.objects.get(id=result_id)
     return render(request, "main/test_result.html", context)
 
+
 @login_required
 def view_test_session(request, test_session_id=None):
     context = dict()
@@ -148,22 +167,26 @@ def view_test_session(request, test_session_id=None):
         context['test_session'] = TestSession.objects.get(id=test_session_id)
     return render(request, "main/test_session.html", context)
 
+
 @login_required
 def show_test_charters(request, version_id=None):
     context = dict()
     if version_id:
-        context['all_test_charters'] = TestCharter.objects.filter(versions__id=version_id)
+        context['all_test_charters'] = TestCharter.objects.filter(
+                versions__id=version_id)
     else:
         context['all_test_charters'] = TestCharter.objects.all()
     return render(request, "main/test_charters.html",
-    context)
+                  context)
+
 
 @login_required
 def show_test_charter(request, test_id):
     context = dict()
     context['test_charter'] = TestCharter.objects.get(id=test_id)
     return render(request, "main/test_charter_details.html",
-    context)
+                  context)
+
 
 @login_required
 def new_test_charter(request):
@@ -182,6 +205,7 @@ def new_test_charter(request):
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def update_test_charter(request, id):
     context = dict()
@@ -196,28 +220,32 @@ def update_test_charter(request, id):
             form.save_m2m()
             return redirect(show_test_charters)
     else:
-        form = TestCharterForm(instance = test_charter)
+        form = TestCharterForm(instance=test_charter)
     context['form'] = form
     context['test_charter'] = test_charter
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_test_charter(request, id):
-    test_charter  = TestCharter.objects.get(id=id)
+    test_charter = TestCharter.objects.get(id=id)
     if test_charter:
         test_charter.delete()
         return redirect(show_test_charters)
+
 
 @login_required
 def show_test_groups(request, version_id=None):
     context = dict()
     if version_id:
-        context['all_test_groups'] = TestGroup.objects.filter(version__id=version_id)
+        context['all_test_groups'] = TestGroup.objects.filter(
+                version__id=version_id)
         context['version_id'] = version_id
     else:
         context['all_test_groups'] = TestGroup.objects.all()
     return render(request, "main/test_groups.html",
-    context)
+                  context)
+
 
 @login_required
 def show_test_group(request, group_id):
@@ -244,10 +272,15 @@ def show_test_group(request, group_id):
             test_sessions.append(None)
             session_ids.append(None)
     context['test_group'] = group
-    context['test_cases'] = zip(group.test_cases.all(), test_results, result_ids)
-    context['test_charters'] = zip(group.test_charters.all(), test_sessions, session_ids)
+    context['test_cases'] = zip(group.test_cases.all(),
+                                test_results,
+                                result_ids)
+    context['test_charters'] = zip(group.test_charters.all(),
+                                   test_sessions,
+                                   session_ids)
     return render(request, "main/test_group_details.html",
-    context)
+                  context)
+
 
 @login_required
 def new_test_group(request, version_id):
@@ -257,7 +290,7 @@ def new_test_group(request, version_id):
         form = TestGroupForm(request.POST)
         if form.is_valid():
             test_group = form.save(commit=False)
-            test_group.version =  Version.objects.get(id=version_id)
+            test_group.version = Version.objects.get(id=version_id)
             test_group.save()
             form.save_m2m()
             return show_test_groups(request, version_id=test_group.version.id)
@@ -265,6 +298,7 @@ def new_test_group(request, version_id):
         form = TestGroupForm()
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
+
 
 @login_required
 def update_test_group(request, id):
@@ -279,24 +313,28 @@ def update_test_group(request, id):
             form.save_m2m()
             return show_test_groups(request, version_id=test_group.version.id)
     else:
-        form = TestGroupForm(instance = test_group)
+        form = TestGroupForm(instance=test_group)
     context['form'] = form
     context['test_group'] = test_group
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_test_group(request, id):
-    test_group  = TestGroup.objects.get(id=id)
+    test_group = TestGroup.objects.get(id=id)
     if test_group:
         test_group.delete()
         return redirect(show_test_groups)
+
 
 @login_required
 def show_versions(request):
     context = dict()
     context['all_versions'] = Version.objects.all()
     return render(request, "main/versions.html",
-    context)
+                  context)
+
+
 @login_required
 def new_version(request):
     context = dict()
@@ -312,6 +350,7 @@ def new_version(request):
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def update_version(request, id):
     context = dict()
@@ -323,24 +362,27 @@ def update_version(request, id):
             form.save()
             return redirect(show_versions)
     else:
-        form = VersionForm(instance = version)
+        form = VersionForm(instance=version)
     context['form'] = form
     context['version'] = version
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_version(request, id):
-    version  = Version.objects.get(id=id)
+    version = Version.objects.get(id=id)
     if version:
         version.delete()
     return redirect(show_versions)
+
 
 @login_required
 def show_personas(request):
     context = dict()
     context['all_personas'] = Persona.objects.all()
     return render(request, "main/personas.html",
-    context)
+                  context)
+
 
 @login_required
 def new_persona(request):
@@ -355,7 +397,8 @@ def new_persona(request):
     else:
         form = PersonaForm()
     context['form'] = form
-    return render(request, 'main/entity_form.html', context) 
+    return render(request, 'main/entity_form.html', context)
+
 
 @login_required
 def update_persona(request, id):
@@ -368,17 +411,19 @@ def update_persona(request, id):
             form.save()
             return show_personas(request)
     else:
-        form = PersonaForm(instance = persona)
+        form = PersonaForm(instance=persona)
     context['form'] = form
     context['persona'] = persona
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_persona(request, id):
-    persona  = Persona.objects.get(id=id)
+    persona = Persona.objects.get(id=id)
     if persona:
         persona.delete()
     return redirect(show_personas)
+
 
 @login_required
 def show_defects(request, version_id=None):
@@ -388,14 +433,16 @@ def show_defects(request, version_id=None):
     else:
         context['all_defects'] = Defect.objects.all()
     return render(request, "main/defects.html",
-        context)
+                  context)
+
 
 @login_required
 def show_defect(request, defect_id):
     context = dict()
     context['defect'] = Defect.objects.get(id=defect_id)
     return render(request, "main/defect_details.html",
-    context)
+                  context)
+
 
 @login_required
 def new_defect(request):
@@ -414,6 +461,7 @@ def new_defect(request):
     context['form'] = form
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def update_defect(request, id):
     context = dict()
@@ -428,14 +476,15 @@ def update_defect(request, id):
             form.save_m2m()
             return show_defects(request)
     else:
-        form = DefectForm(instance = defect)
+        form = DefectForm(instance=defect)
     context['form'] = form
     context['defect'] = defect
     return render(request, 'main/entity_form.html', context)
 
+
 @login_required
 def delete_defect(request, id):
-    defect  = Defect.objects.get(id=id)
+    defect = Defect.objects.get(id=id)
     if defect:
         defect.delete()
     return redirect(show_defects)
